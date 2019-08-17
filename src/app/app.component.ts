c1397c9adcbc512bce6724adc796a3162247a473
Venkatesh R <venkatesh.br@gmail.com>
2019-08-17 11:38:25 +0530

    updated news component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,22 +11,4 @@ export class AppComponent {
   title = 'ishirock';
 
 
-  mArticles: Array<any>;
-  mSources: Array<any>;
-
-  constructor(private newsapi: NewsApiService) {
-    console.log('app component constructor called');
-  }
-
-  ngOnInit() {
-    //load articles
-    this.newsapi.initArticles().subscribe(data => this.mArticles = data['articles']);
-    //load news sources
-    this.newsapi.initSources().subscribe(data => this.mSources = data['sources']);
-  }
-
-  searchArticles(source) {
-    console.log("selected source is: " + source);
-    this.newsapi.getArticlesByID(source).subscribe(data => this.mArticles = data['articles']);
-  }
 }
